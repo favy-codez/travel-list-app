812e037b2e5454ca295388c1ea8e69ac036005f6
favycodezz <ezeliorafavour81@gmail.com>
2024-05-22 16:49:28 -0700

    added readme

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 ### Here are some of the things I learnt during the course of this project
-**To use emoji on windows -**  windows + .
+- **To use emoji on windows -**  windows + .
+- **How to use ternary operator to set some styles -** 
+```
+<li>
+      <span style={item.packed ? {textDecoration : "line-through"} : {}}>
+        {item.quantity} {item.description}
+      </span>
+      <button>❌</button>
+</li>
+```
--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+import "./App.css";
 
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
@@ -6,62 +6,59 @@ const initialItems = [
   { id: 3, description: "Charger", quantity: 3, packed: true },
 ];
 
-
-
 export default function App() {
   return (
     <div className="app">
-      <Logo/>
-      <Form/>
-      <PackingList/>
-      <Stat/>
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stat />
     </div>
   );
 }
 
-
-function Logo(){
-// to get emoji windows + .
-return(
-  <h1>🌴  Far Away 💼</h1>
-)
+function Logo() {
+  // to get emoji windows + .
+  return <h1>🌴 Far Away 💼</h1>;
 }
 
-function Form(){
-  return(
-    <div className='add-form'>
+function Form() {
+  return (
+    <div className="add-form">
       <h3>What do you need for your trip?</h3>
     </div>
   );
 }
 
-function PackingList(){
-  return(
+function PackingList() {
+  return (
     // to render list, we use the map method on the array
-    <div className='list'>
+    <div className="list">
       <ul>
-      {initialItems.map(item => 
-      // name of the component, prop, object
-        <Item item={item} key={item.id}/>
-      )}
-    </ul>
+        {initialItems.map((item) => (
+          // name of the component, prop, object
+          <Item item={item} key={item.id} />
+        ))}
+      </ul>
     </div>
   );
 }
 
-function Item({item}){
-  return(
+function Item({ item }) {
+  return (
     <li>
-      <span>{item.quantity} {item.description}</span>
+      <span style={item.packed ? {textDecoration : "line-through"} : {}}>
+        {item.quantity} {item.description}
+      </span>
       <button>❌</button>
     </li>
-  )
+  );
 }
 
-function Stat(){
-  return(
-     <footer className='stats'>
+function Stat() {
+  return (
+    <footer className="stats">
       <em>👜 You have X items on your list, and you already packed X (x%)</em>
-     </footer>
+    </footer>
   );
 }
